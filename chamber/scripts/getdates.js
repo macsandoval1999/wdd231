b902de1e81a71447afa2378d679f0ac296e3aeea
macsandoval1999 <msandoval2@byupathway.edu>
2025-11-10 18:13:04 -0700

    Test Update: spaced out media bar in larger footer, made footer text larger

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -5,7 +5,7 @@ const today = new Date(); // Create a new Date object with the current date and 
 
 // Get current year
 //Used in: footer
-const year = document.querySelector("#currentyear"); // Select the element with id 'currentyear'
+const year = document.querySelector("#current-year"); // Select the element with id 'currentyear'
 year.innerHTML = `${today.getFullYear()}`; // Set its inner HTML to the year stored in 'today'
 
 // Alternative method to get year using textContent
@@ -14,7 +14,7 @@ year.innerHTML = `${today.getFullYear()}`; // Set its inner HTML to the year sto
 
 // Set last modified date
 //Used in: footer
-const lastModified = document.querySelector('#lastModified'); // Select the element with id 'lastModified'
+const lastModified = document.querySelector('#last-modified'); // Select the element with id 'lastModified'
 lastModified.innerHTML = `Last Modification: ${document.lastModified}`; // Set its inner HTML to the last modified date of the document
 
 
